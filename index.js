3ad424af32cf7307d7f7aa4a355bff3441159e4d
jones <pnduru6@gmail.com>
2024-12-14 11:59:48 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+// index.js
+
+// companyName should be a const and set to 'Scuber'
+const companyName = 'Scuber';
+
+// mostProfitableNeighborhood should be a let and set to 'Chelsea'
+let mostProfitableNeighborhood = 'Chelsea';
+
+// companyCeo should be a let and set to 'Susan Smith'
+const companyCeo = 'Sally';
+
